367cd377bbd7311d2c12d9cb0f80e800e6fdaf1f
kamesen99 <kamesen@gmail.com>
2022-11-03 02:59:37 -0700

    Centering elements

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,14 +2,16 @@
 export default function Hero() {
     return(
         <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row">
-            <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
-            <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
-            <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-            <button className="btn btn-primary">Get Started</button>
+            <div className="hero-content flex-col lg:flex-row">
+                <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />
+                <h1 className="text-5xl font-bold">Welcome to the Domain</h1>
+                <div  className="flex justify-center items-center">
+                    <p className="py-6">Stay tuned to this space for upcoming features and changes.</p>
+                </div>
+                <div className="flex justify-center">
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
             </div>
-        </div>
         </div>
     )
 }
