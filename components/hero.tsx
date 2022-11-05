e6de425e2ee0e2919253b7a83d314583c4094df1
kamesen99 <kamesen@gmail.com>
2022-11-05 02:22:01 -0700

    Moving hero content up

--- components/hero.tsx
+++ components/hero.tsx
@@ -19,16 +19,16 @@ export default function Hero() {
 
     return(
         <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row">
+            <div className="hero-content flex-col lg:flex-row relative -top-8">
                 {!effect && <img src="https://placeimg.com/260/400/arch" className="max-w-sm rounded-lg shadow-2xl" />}
                 {/*<h1 className="text-5xl font-bold">Welcome to the Domain</h1>*/}
                 {/*<audio controls src="/sound.mp3" />*/}
                 
                 <div  className="justify-center items-center lg:pl-10">     
                     {!effect && <p className="py-6 items-center">We both know why you're here ;)</p>}
-                    <div className="flex justify-center pb-2">
+                    <div className="flex justify-center pb-3">
                     {!effect && <div className="animate-bounce bg-white dark:bg-slate-800 p-2 w-10 h-10 ring-1 ring-slate-900/5 dark:ring-slate-200/20 shadow-lg rounded-full flex items-center justify-center">
-                        <svg className="w-6 h-6 text-blue-500" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                        <svg className="w-6 h-6 text-blue-500" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
                         <path d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
                         </svg>
                         </div>
