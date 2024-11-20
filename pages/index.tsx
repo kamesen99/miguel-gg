b05af3fcf736c2dbb8e494391907175a7e827a0b
kamesen99 <kamesen@gmail.com>
2024-11-19 22:47:21 -0800

    Fix Hero call

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import Hero from '../components/Hero';
-import AboutMe from '../components/AboutMe';
-import WorkExperience from '../components/WorkExperience';
+import Hero from '@components/Hero'
+import AboutMe from '@components/AboutMe';
+import WorkExperience from '@components/WorkExperience';
 // import Skills from '../components/Skills';
 // import Contact from '../components/Contact';
 
